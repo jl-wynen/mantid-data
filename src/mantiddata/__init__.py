6151cf3469371d8223bfb66c96d31b59bcc2f613
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-04-01 15:48:04 +0200

    Add __version__

--- src/mantiddata/__init__.py
+++ src/mantiddata/__init__.py
@@ -1,6 +1,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 mantiddata contributors (https://github.com/mantid-data)
 
+import importlib.metadata
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
 from importlib import resources
 from pathlib import Path
 from typing import Optional, Union
