9ddafe15ce6b8c4c4cc5c9aa5de7fcc3e808f876
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-04-01 13:02:02 +0200

    Add basic package pip config

---
+++ src/mantiddata/__init__.py
@@ -0,0 +1,20 @@
+from importlib import resources
+from pathlib import Path
+from typing import Optional, Union
+
+import pooch
+
+
+def create(registry_file: Optional[Union[Path, str]]=None) -> pooch.Pooch:
+    inv = pooch.create(
+        path=pooch.os_cache('mantid/test_data'),
+        env='MANTID_DATA_DIR',
+        retry_if_failed=3,
+        base_url='https://testdata.mantidproject.org/ftp/external-data/MD5/',
+        registry=None
+    )
+    if registry_file:
+        inv.load_registry(registry_file)
+    else:
+        inv.load_registry(resources.path('mantiddata', 'registry.txt'))
+    return inv
